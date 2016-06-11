729a59d19093c1fbab53afc9e4d0683a429fd29a
Grindary <zcz95116@hotmail.com>
2016-06-11 11:33:12 +0800

    Use a new yml for kitti.

--- experiments/scripts/kitti.sh
+++ experiments/scripts/kitti.sh
@@ -5,4 +5,4 @@ time ./tools/train_kitti.py --gpu 0 \
     --solver models/kitti/faster_rcnn_end2end/solver.prototxt \
     --iters 70000 \
     --weights data/imagenet_models/VGG16.v2.caffemodel \
-    --cfg experiments/cfgs/faster_rcnn_end2end.yml
+    --cfg experiments/cfgs/kitti.yml
